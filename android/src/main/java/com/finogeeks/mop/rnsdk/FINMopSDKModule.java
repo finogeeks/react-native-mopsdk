69cf6fb0d7ac4a2af3c004eaae3a1e901929aa18
15852765247 <lijun@finogeeks.com>
2024-01-04 19:31:45 +0800

    添加hashmap依赖

--- android/src/main/java/com/finogeeks/mop/rnsdk/FINMopSDKModule.java
+++ android/src/main/java/com/finogeeks/mop/rnsdk/FINMopSDKModule.java
@@ -52,6 +52,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
