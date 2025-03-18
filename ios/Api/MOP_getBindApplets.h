ff8e17a3a87cc6ee1eeaf470b943c875c6750fcf
wanghualei <wanghualei@finogeeks.com>
2025-03-18 10:59:42 +0800

    支持ios 侧的getBindApplets api

---
+++ ios/Api/MOP_getBindApplets.h
@@ -0,0 +1,17 @@
+//
+//  MOP_getBindApplets.h
+//  FINMopSDK
+//
+//  Created by fino on 2025/3/18.
+//  Copyright © 2025 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MOP_getBindApplets : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Api/MOP_getBindApplets.m
@@ -0,0 +1,13 @@
+//
+//  MOP_getBindApplets.m
+//  FINMopSDK
+//
+//  Created by fino on 2025/3/18.
+//  Copyright © 2025 Facebook. All rights reserved.
+//
+
+#import "MOP_getBindApplets.h"
+
+@implementation MOP_getBindApplets
+
+@end
